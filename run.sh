1fb8cf785e5d4b596fc5a1d2e91edce012613138
Cojan van Ballegooijen <cojan@redhat.com>
2014-12-30 18:09:24 +0100

    Modified run.sh to include JBoss Fuse

--- run.sh
+++ run.sh
@@ -1,38 +1,57 @@
 #!/bin/sh 
-DEMO="JBoss DV and Fuse Integration Demo"
-AUTHORS="Kenny Peeples, Bill Kemp, Cojan"
-PROJECT="git@github.com:kpeeples/dv-brms-integration-demo.git"
-PRODUCT="JBoss BRMS & JBoss DV Integration Demo"
+DEMO="JBoss Data Virtualization & Fuse Integration Demo" 
+AUTHORS="Kenny Peeples, Bill Kemp, Cojan van Ballegooijen"
+PROJECT="git@github.com:kpeeples/dv-fuse-integration-demo.git"
+PRODUCT="JBoss DV & Fuse Integration Demo"
 JBOSS_HOME=./target/jboss-eap-6.1
 JBOSS_HOME_DV=./target/dv/jboss-eap-6.1
+JBOSS_HOME_FUSE=./target/jboss-fuse-6.1.0.redhat-379
 SERVER_DIR=$JBOSS_HOME/standalone/deployments/
 SERVER_CONF=$JBOSS_HOME/standalone/configuration/
 SERVER_BIN=$JBOSS_HOME/bin
 SERVER_BIN_DV=$JBOSS_HOME_DV/bin
+SERVER_BIN_FUSE=$JBOSS_HOME_FUSE/bin
+SERVER_DIR=$JBOSS_HOME/standalone/deployments/
 SERVER_CONF_DV=$JBOSS_HOME_DV/standalone/configuration/
+SERVER_CONF_FUSE=$JBOSS_HOME_FUSE/etc
 SRC_DIR=./software
 DV_SUPPORT_DIR=./support/dv-support
+FUSE_SUPPORT_DIR=./support/fuse-support
 PRJ_DIR=./projects
-FUSE=
+FUSE=jboss-fuse-full-6.1.0.redhat-379.zip
 DV=jboss-dv-installer-6.0.0.GA-redhat-4.jar
-FUSE_VERSION=6.0.3
+FUSE_VERSION=6.1.0
 DV_VERSION=6.0.0
 
 # wipe screen.
 clear 
 
-echo  
-echo "Setting up ${DEMO}" 
-echo "brought to you by,"   
-echo "${AUTHORS}"
-echo "${PROJECT}"
+echo
+echo "########################################################################"
+echo "##                                                                    ##"   
+echo "##  Setting up the ${DEMO}  ##"
+echo "##                                                                    ##"   
+echo "##                                                                    ##"   
+echo "##   ####  #        #        ####  #  #   ###  ####                   ##"
+echo "##   #   #  #      #     #   #     #  #  #     #                      ##"
+echo "##   #   #   #    #     ###  ###   #  #   ##   ###                    ##"
+echo "##   #   #    #  #       #   #     #  #     #  #                      ##"
+echo "##   ####      #             #     ####  ###   ####                   ##"
+echo "##                                                                    ##"   
+echo "##                                                                    ##"   
+echo "##  brought to you by,                                                ##"   
+echo "##             ${AUTHORS}       ##"
+echo "##                                                                    ##"   
+echo "##  ${PROJECT}              ##"
+echo "##                                                                    ##"   
+echo "########################################################################"
 echo
 
 command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
 
 # make some checks first before proceeding.	
 if [ -r $SRC_DIR/$DV ] || [ -L $SRC_DIR/$DV ]; then
-	echo JBoss product sources, $DV present...
+	    echo JBoss product sources, $DV present...
 		echo
 else
 		echo Need to download $DV package from the Customer Portal 
@@ -41,11 +60,24 @@ else
 		exit
 fi
 
-# Move the old JBoss instance, if it exists, to the OLD position.
-if [ -x $JBOSS_HOME ]; then
-		echo "  - existing JBoss product install detected and removed..."
+# make some checks first before proceeding.	
+if [ -r $SRC_DIR/$FUSE ] || [ -L $SRC_DIR/$FUSE ]; then
+	    echo JBoss product sources, $FUSE present...
 		echo
-		rm -rf ./target
+else
+		echo Need to download $FUSE package from the Customer Portal 
+		echo and place it in the $SRC_DIR directory to proceed...
+		echo
+		exit
+fi
+
+# Remove JBoss product installation if exists.
+if [ -x target ]; then
+	echo "  - existing JBoss product installation detected..."
+	echo
+	echo "  - removing existing JBoss product installation..."
+	echo
+	rm -rf target
 fi
 
 read -p "Starting DV Install <hit return or wait 5 seconds>" -t 2
@@ -79,7 +111,60 @@ echo "  - setting up dv standalone.xml configuration adjustments..."
 echo
 cp $DV_SUPPORT_DIR/standalone.dv.xml $SERVER_CONF_DV/standalone.xml
 
-# Final instructions to user to start and run demo.                                                                                     ="
-echo "$DEMO Setup Complete."
+if [ -x target ]; then
+  # Unzip the JBoss Fuse instance.
+  echo Installing JBoss Fuse $FUSE_VERSION
+  echo
+  unzip -q -d target $SRC_DIR/$FUSE
+else
+	echo
+	echo Missing target directory, stopping installation.
+	echo 
+	exit
+fi
+
+echo "  - enabling demo accounts logins in users.properties file..."
+echo
+cp $FUSE_SUPPORT_DIR/users.properties $SERVER_CONF_FUSE
+
+# Final instructions to user to start and run demo.                                                                  
+echo
+echo "==========================================================================================="
+echo "=                                                                                         ="
+echo "=  You can now start the JBoss Data Virtualization with:                                  ="
+echo "=                                                                                         ="
+echo "=        $SERVER_BIN/standalone.sh                                         ="
+echo "=                                                                                         ="
+echo "=    - login JBoss Data Virtualization at:                                                ="
+echo "=                                                                                         ="
+echo "=        http://localhost:8080/ (u:admin/p:redhat1!)                                      ="
+echo "=                                                                                         ="
+echo "=  Deploying the camel route in JBoss Fuse as follows:                                    ="
+echo "=                                                                                         ="
+echo "=    - add fabric server passwords for Maven Plugin to your ~/.m2/settings.xml            =" 
+echo "=      file the fabric server's user and password so that the maven plugin can            ="
+echo "=      login to the fabric. fabric8.upload.repoadminadmin                                 ="
+echo "=                                                                                         ="
+echo "=    - start the JBoss Fuse with:                                                         ="
+echo "=                                                                                         ="
+echo "=        $SERVER_BIN_FUSE/fuse                                    ="
+echo "=                                                                                         ="
+echo "=    - start up fabric in fuse console: fabric:create --wait-for-provisioning             ="
+echo "=                                                                                         ="
+echo "=    - run 'mvn fabric8:deploy' from projects/brms-fuse-integration/simpleRoute           ="
+echo "=                                                                                         ="
+echo "=    - login to Fuse management console at:                                               ="
+echo "=                                                                                         ="
+echo "=        http://localhost:8181    (u:admin/p:admin)                                       ="
+echo "=                                                                                         ="
+echo "=    - connect to root container with login presented by console  (u:admin/p:admin)       ="
+echo "=                                                                                         ="
+echo "=    - create container name c1 and add BPMSuiteFuse profile (see readme for screenshot)  ="
+echo "=                                                                                         ="
+echo "=    - open c1 container to view route under 'DIAGRAM' tab                                ="
+echo "=                                                                                         ="                   
+echo "=                                                                                         ="
+echo "=   $DEMO Setup Complete.                     ="
+echo "==========================================================================================="
 echo
 
